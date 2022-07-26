e757a4b290dcf03024f113abe147d53a3d694b75
Vlad Nesvit <65105957+vladn255@users.noreply.github.com>
2022-07-26 14:49:37 +0400

    m6-t3 updates article exists check util

--- src/service/middlewares/articleExists.js
+++ src/service/middlewares/articleExists.js
@@ -4,7 +4,7 @@ const {HttpCode} = require(`../../constants`);
 
 module.exports = (service) => async (req, res, next) => {
   const {articleId} = req.params;
-  const article = await service.findOne(articleId);
+  const article = await service.findOne({id: articleId, withComments: true});
 
   if (!article) {
     return res.status(HttpCode.NOT_FOUND).send(`Article with the id=${articleId} not found`);
