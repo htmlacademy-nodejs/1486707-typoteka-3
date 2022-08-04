328ff53c38f9ee5aef94a0a7b0d04f49ab287bf4
Vlad Nesvit <65105957+vladn255@users.noreply.github.com>
2022-08-04 18:24:40 +0400

    m7-t1 adds validation schemas

--- src/service/middlewares/articleValidator.js
+++ src/service/middlewares/articleValidator.js
@@ -1,18 +1,46 @@
 'use strict';
 
+const Joi = require(`joi`);
 const {HttpCode} = require(`../../constants`);
 
 const articleKeys = [`title`, `announce`, `articleText`, `categories`];
 
-module.exports = (req, res, next) => {
-  const newArticle = req.body;
-  const keys = Object.keys(newArticle);
-  const keysExist = articleKeys.every((key) => keys.includes(key));
+const ErrorArticleMessage = {
+  TITLE_MIN: `Заголовок должен содержать не меньше 30 симоволов`,
+  TITLE_MAX: `Заголовок должен содержать не больше 250 символов`,
+  ANNOUNCE_MIN: `Анонс должен содержать не меньше 30 символов`,
+  ANNOUNCE_MAX: `Анонс должен содержать не больше 250 символов`,
+  TEXT: `Текст должен содержать не больше 1000 символов`,
+  CATEGORIES: `Не выбрана ни одна категория`
+};
 
-  if (!keysExist) {
-    res.status(HttpCode.BAD_REQUEST).send(`Bad request`);
-    return;
+const schema = Joi.object({
+  categories: Joi.array().items(
+      Joi.number().integer().positive().messages({
+        'number.base': ErrorArticleMessage.CATEGORIES
+      })
+  ).min(1).required(),
+  title: Joi.string().min(30).max(100).messages({
+    'string.min': ErrorArticleMessage.TITLE_MIN,
+    'string.max': ErrorArticleMessage.TITLE_MAX
+  }).required(),
+  announce: Joi.string().min(30).max(250).messages({
+    'string.min': ErrorArticleMessage.ANNOUNCE_MIN,
+    'string.max': ErrorArticleMessage.ANNOUNCE_MAX
+  }).required(),
+  articleText: Joi.string().max(1000).messages({
+    'string.max': ErrorArticleMessage.TEXT
+  }).required(),
+  picture: Joi.string()
+});
+
+module.exports = (req, res, next) => {
+  const article = req.body;
+  const {error} = schema.validate(article, {abortEarly: false});
+
+  if (error) {
+    return res.status(HttpCode.BAD_REQUEST).send(error.details.map((err) => err.message)).join(`\n`);
   }
 
-  next();
+  return next();
 };
--- src/service/middlewares/commentValidator.js
+++ src/service/middlewares/commentValidator.js
@@ -1,18 +1,25 @@
 'use strict';
 
+const Joi = require(`joi`);
 const {HttpCode} = require(`../../constants`);
 
-const commentKeys = [`avatar`, `name`, `surname`, `text`];
+const ErrorCommentMessage = {
+  TEXT: `Комментарий содержит меньше 20 символов`
+};
+
+const schema = Joi.object({
+  text: Joi.string().min(20).required().messages({
+    'string.min': ErrorCommentMessage.TEXT
+  })
+});
 
 module.exports = (req, res, next) => {
-  const newComment = req.body;
-  const keys = Object.keys(newComment);
-  const keysExist = commentKeys.every((key) => keys.includes(key));
+  const comment = req.body;
 
-  if (!keysExist) {
-    res.status(HttpCode.BAD_REQUEST).send(`Bad request`);
-    return;
+  const {error} = schema.validate(comment, {abortEarly: false});
+  if (error) {
+    return res.status(HttpCode.BAD_REQUEST).send(error.details.map((err) => err.message).join(`\n`));
   }
 
-  next();
+  return next();
 };
